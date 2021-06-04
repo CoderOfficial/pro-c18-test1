6e4a260bd51f843d3b873ad4060698ae3f0872bc
CoderOfficial <84549680+CoderOfficial@users.noreply.github.com>
2021-06-03 19:38:48 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -36,13 +36,13 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight);
   
-  trex = createSprite(windowWidth-50,windowHeight-50,20,50);
+  trex = createSprite(50,180,20,50);
   
   trex.addAnimation("running", trex_running);
   trex.addImage("collided", trex_collided);
   trex.scale = 0.5;
   
-  ground = createSprite(windowWidth-50,windowHeight-50,400,20);
+  ground = createSprite(250,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
